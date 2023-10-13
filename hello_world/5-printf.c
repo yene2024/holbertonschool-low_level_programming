714cfa5b02ef83ee7a09d7b3f554f78f73b2aa23
yene2024 <yeneishla1524@gmail.com>
2023-10-13 08:51:56 -0400

    Task 5: Hello, printf

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,2 +1,12 @@
-#!/bin/bash
-printf("with proper grammar, but the outcome is a piece of art,");
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+       	return (0);
+}
