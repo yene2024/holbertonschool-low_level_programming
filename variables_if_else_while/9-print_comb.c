a0522b7f0a7ff338628c19b12385afcbc380b3ec
yene2024 <yeneishla1524@gmail.com>
2023-10-16 19:46:40 -0400

    Prints all combination of single digit nunmber

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+*main - Print the alphabetic a-z
+*Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+	    putchar('0' + i);
+
+		if (i < 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
