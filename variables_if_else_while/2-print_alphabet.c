5d1c568e8e98f304fb4a7c5546041402277b088f
yene2024 <yeneishla1524@gmail.com>
2023-10-16 14:36:57 -0400

    Print alphabet

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* main - Prints the alphabetic */
+
+/* return: always (success) */
+int main(void)
+{
+	int n;
+
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	putchar(c);
+	putchar('\n');
+	return (0);
+}
