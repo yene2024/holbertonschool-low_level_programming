014e73f3edec522f9f69bc919d40cb0930846238
yene2024 <yeneishla1524@gmail.com>
2023-10-16 14:52:28 -0400

    Print alphabet.c

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -1,9 +1,14 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/* main - Prints the alphabetic */
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+*main - Print the alphabetic a-z
+*Return: Always 0 (Success)
+ */
 
-/* return: always (success) */
 int main(void)
 {
 	char c;
