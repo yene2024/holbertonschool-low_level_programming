3d6c25a253c80becf9ee089bfe9468366ceee5ee
yene2024 <yeneishla1524@gmail.com>
2023-10-17 10:52:30 -0400

    Writing a program that print putchar.

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 /**
-*main - Print the alphabetic a-z
+*main - create a main.h
 *Return: Always 0 (Success)
  */
 
 int main(void)
 {
-	printf("_putchar");
+	printf("_putchar\n");
 	return (0);
 }
 
