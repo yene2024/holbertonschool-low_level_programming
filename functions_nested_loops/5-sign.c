f3b090cb787ba06761fb71c5ff9005685f63a60b
yene2024 <yeneishla1524@gmail.com>
2023-10-17 20:54:06 -0400

    Writing a function that print the sign number.

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -12,14 +12,11 @@ int print_sign(int n)
 
 	if (n >= 1)
 	{
-	printf("+");
-	return (1);
+	return (+1);
 	}
 	else if (n == 0)
 	{
-		printf("0");
 		return (0);
 	}
-	printf("-");
 	return (-1);
 }
