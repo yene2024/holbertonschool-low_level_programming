337b3d4f850da04c764d160c3a4ed00962b9f404
yene2024 <yeneishla1524@gmail.com>
2023-10-18 13:19:58 -0400

    Writing a function that computes absolute value.

--- functions_nested_loops/6-abs.c
+++ functions_nested_loops/6-abs.c
@@ -1,8 +1,8 @@
 #include "main.h"
 #include<stdio.h>
 /**
- * _abs(int) - function that computes the absolute value of an integer.
- *
+ * _abs - function that computes the absolute value of an integer.
+ *@i: computes absolute value
  * Return: integer i positive if bigger than 0, return negative otherwise.
  */
 int _abs(int i)
--- functions_nested_loops/6-main.c
+++ functions_nested_loops/6-main.c
@@ -1,11 +1,11 @@
 #include "main.h"
 #include <stdio.h>
-
 /**
- * _abs(int) - function that computes the absolute value of an integer.
- *
- * Return: Always 0.
+ * _abs - function that computes the absolute value of an integer.
+ *@i: computes absolute value
+ * Return: integer i positive if bigger than 0, return negative otherwise.
  */
+
 int main(void)
 {
     int r;
