6591bcb1773c00a49f483261ceb1b3fc85500805
yene2024 <yeneishla1524@gmail.com>
2023-10-18 13:12:19 -0400

    Writing a function that print all natural numbers

---
+++ functions_nested_loops/11-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *print_to_98 - function that prints all natural numbers
+ *@n: print all natural numbers
+ */
+
+int main(void)
+{
+    print_to_98(0);
+    print_to_98(98);
+    print_to_98(111);
+    print_to_98(81);
+    print_to_98(-10);
+    return (0);
+}
