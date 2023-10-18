2478847c14010229f62a21e73e1e5d98860466d1
yene2024 <yeneishla1524@gmail.com>
2023-10-17 20:46:55 -0400

    task 4: Writing a function that print the sign number

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 /**
  * print_sign - function that prints the sign of a number.
- *
+ *@n: variable to print
  * Return: 1 and prints + if n is greater than zero
  * Returns 0 and prints 0 if n is zero
  * Returns -1 and prints - if n is less than zero
