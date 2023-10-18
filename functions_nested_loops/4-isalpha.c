d8fe8e0877f62b20af2a921378463daf7bd2765a
yene2024 <yeneishla1524@gmail.com>
2023-10-17 20:41:14 -0400

    task 4: Writing a function that checks for alphabetic character.

--- functions_nested_loops/4-isalpha.c
+++ functions_nested_loops/4-isalpha.c
@@ -2,13 +2,14 @@
 /**
  * _isalpha - main function
  *
- * Return: 1 if c is a letter, lowercase or uppercase
  * @c: character to compare
+ *
  * Return: 1 if true, 0 if false
+ *
  */
 int _isalpha(int c)
 {
-	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c<= 'Z'))
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
 	{
 		return (1);
 	}
