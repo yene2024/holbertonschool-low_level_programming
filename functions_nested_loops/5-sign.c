f00366c1b4f141f76d74bf256197a03ea066b7d2
yene2024 <yeneishla1524@gmail.com>
2023-10-17 21:33:43 -0400

    task 5: Writing a function that print the sign number.

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -12,19 +12,17 @@ int print_sign(int n)
 
 	if (n >= 1)
 	{
-	putchar('1');
-	putchar('+');
+	putchar(43);
 	return (1);
 	}
-	else if (n == 0)
+	else if (n < 0)
 	{
-		putchar('0');
-		return (0);
+		putchar(45);
+		return (-1);
 	}
 	else
 	{
-		putchar('1');
-		putchar('-');
-		return (-1);
+		putchar(48);
+		return (0);
 	}
 }
