c0d48d0c2d3895e2863554eca387f95f46daa877
yene2024 <yeneishla1524@gmail.com>
2023-10-18 12:09:16 -0400

    Writing a function that adds two integers

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * add - function that adds two integers.
+ * @a: number integers
+ * @b: number integers
+ * Return: the result
+ */
+int add(int a, int b)
+	{
+		return (a + b);
+	}
---
+++ functions_nested_loops/10-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * add - function that adds two integers.
+ * @a: number integers
+ * @b: number integers
+ * Return: the result
+ */
+int main(void)
+{
+    int n;
+
+    n = add(89, 9);
+    printf("%d\n", n);
+    return (0);
+}
