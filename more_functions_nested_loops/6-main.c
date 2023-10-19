421c8a119cbe06e589636874720c1e85e4981ed3
yene2024 <yeneishla1524@gmail.com>
2023-10-19 14:47:01 -0400

    Writing a function that draws a straight line

---
+++ more_functions_nested_loops/6-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_line - function that draws
+ *@n: The number of lines to draw
+ * Return: empty
+ */
+
+int main(void)
+{
+    print_line(0);
+    print_line(2);
+    print_line(10);
+    print_line(-4);
+    return (0);
+}
+
