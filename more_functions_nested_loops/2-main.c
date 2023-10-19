b195876c3dd0cff2d99271636585ac1de53b7237
yene2024 <yeneishla1524@gmail.com>
2023-10-19 11:27:16 -0400

    Writing a function that multiplies integers

---
+++ more_functions_nested_loops/2-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * mul - function that multiplies
+ *@a: int multiplate a b.
+ *@b: int multiplaate a.
+ * Return: Always 0.
+ */
+int main(void)
+{
+    printf("%d\n", mul(98, 1024));
+    printf("%d\n", mul(-402, 4096));
+    return (0);
+}
---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * mul - function that multiplies
+ *@a: int multiplate a b.
+ *@b: int multiplaate a.
+ * Return: Always 0.
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
