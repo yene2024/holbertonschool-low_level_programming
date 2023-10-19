bad1685e5b826ec4e306d502b015e71ea7d7134a
yene2024 <yeneishla1524@gmail.com>
2023-10-19 13:10:06 -0400

    Task 3: The numbers speak for themselves

---
+++ more_functions_nested_loops/3-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * print_numbers - function that prints numbers.
+ *
+ * Return: The numbers since 0 to 9.
+ */
+
+int main(void)
+{
+    print_numbers();
+    return (0);
+}
