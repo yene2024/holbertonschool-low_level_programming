aca3c9dabc9b37d72ed90c05eda111e8e9089b27
yene2024 <yeneishla1524@gmail.com>
2023-10-19 13:29:44 -0400

    print numbers commit

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -5,11 +5,11 @@
 */
 void print_numbers(void)
 {
-    int i;
+	int i;
 
-    for (i = '0'; i <= '9'; i++)
-    {
-        _putchar(i);
-    }
-    _putchar('\n');
+	for (i = '0'; i <= '9'; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
 }
