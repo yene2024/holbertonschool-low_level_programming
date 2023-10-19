2b6d93ac4b0daf89d9284e4a917118815e4b533c
yene2024 <yeneishla1524@gmail.com>
2023-10-19 14:21:35 -0400

    Writing a function that print 10 times numbers

---
+++ more_functions_nested_loops/5-main.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * more_numbers - function that print 10 times
+ *Return: 10 times of number since 0 up yo 14
+ */
+int main(void)
+{
+    more_numbers();
+    return (0);
+}
---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * more_numbers - function that print 10 times
+ *Return: 10 times of number since 0 up yo 14
+ */
+void more_numbers(void)
+{
+	int x, y;
+
+	for (x = 0; x < 10; x++)
+	{
+	for (y = 0; <= 14; y++)
+	{
+	if (y > 9)
+	{
+	_putchar((y / 10) + '0');
+	}
+	_putchar((y % 10) + '0');
+	}
+	_putchar('\n');
+	}
+}
