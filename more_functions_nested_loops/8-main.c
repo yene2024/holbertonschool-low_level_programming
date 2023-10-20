2bb328d3afd3a6a228770415de2392e2398279db
yene2024 <yeneishla1524@gmail.com>
2023-10-20 14:37:07 -0400

    Writing a function that print a square

---
+++ more_functions_nested_loops/8-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_square - function that prints a square
+ *
+ *@size: is the size of square
+ */
+int main(void)
+{
+    print_square(2);
+    print_square(10);
+    print_square(0);
+    return (0);
+}
