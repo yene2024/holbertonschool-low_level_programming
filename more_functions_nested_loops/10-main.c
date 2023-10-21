5175909d4846d79f54d3d9e8920d63ddc855f60a
yene2024 <yeneishla1524@gmail.com>
2023-10-21 18:53:51 -0400

    Writing a function that print a triangle

---
+++ more_functions_nested_loops/10-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_triangle - function that prints triangle.
+ *@size: The size of the triangle.
+ */
+int main(void)
+{
+    print_triangle(2);
+    print_triangle(10);
+    print_triangle(1);
+    print_triangle(0);
+    return (0);
+}
