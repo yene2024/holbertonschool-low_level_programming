d91c5e5db9aea11214f71db317b6b3fdb772488d
yene2024 <yeneishla1524@gmail.com>
2023-10-23 12:56:06 -0400

    Writing a function that takes a pointer

---
+++ pointers_arrays_strings/0-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * reset_to_98 - function that takes a pointer.
+ *@n:parameters
+ */
+int main(void)
+{
+    int n;
+
+    n = 402;
+    printf("n=%d\n", n);
+    reset_to_98(&n);
+    printf("n=%d\n", n);
+    return (0);
+}
---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * reset_to_98 - function that takes a pointer.
+ *@n:parameters
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
