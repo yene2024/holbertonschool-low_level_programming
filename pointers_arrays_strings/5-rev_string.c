b3be8fa69dfe818d946a0806d017efa6d93fec78
yene2024 <yeneishla1524@gmail.com>
2023-10-24 13:25:15 -0400

    task 5

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -7,16 +7,23 @@
  */
 void rev_string(char *s)
 {
-	int length = 0;
-	int i = 0;
+	char tmp;
+	int i, len, len1;
 
-	while (s[length] != '\0')
+	len = 0;
+	len1 = 0;
+
+	while (s[len] != '\0')
 	{
-		length++;
+		len++;
 	}
-	for (i = length - 1; i >= 0; i--)
+	len1 = len - 1;
+
+	for (i = 0; i < len / 2; i++)
 	{
-		_putchar(s[i]);
+		tmp = s[i];
+		s[i] = s[len1];
+		s[len1--] = tmp;
 	}
 }
 
