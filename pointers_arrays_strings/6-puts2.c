6b2f1ed24a92b29c69fa4f8b81edac8d52f7e231
yene2024 <yeneishla1524@gmail.com>
2023-10-24 08:55:19 -0400

    Task 6: writing a function that prints every other character

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -7,12 +7,18 @@
  */
 void puts2(char *str)
 {
-	int i = 0;
+	int len, i;
 
-	for (i = 0; str[i] != '\0'; i += 2)
+	len = 0;
+
+	while (str[len] != '\0')
 	{
-		_putchar(str[i]);
+		len++;
 	}
 
+	for (i = 0; i < len; i += 2)
+	{
+	_putchar(str[i]);
+	}
 	_putchar('\n');
 }
