4cea1281d30f65673bdb919cd6ba47d635c8ba3f
yene2024 <yeneishla1524@gmail.com>
2023-10-24 12:47:28 -0400

    Task 10: Writing a function that convert a string

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,36 +1,46 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * *_atoi - function that convert
- * @s:string to convert
- * Return: returns integer value
+ * _atoi - converts a string to an integer
+ * @s: string to be converted
+ *
+ * Return: the int converted from the string
  */
 int _atoi(char *s)
 {
-	int i = 0;
-	int neg = 0;
-	int num = 0;
-	unsigned int conv = 0;
+	int i, d, n, len, f, digit;
 
-	while (s[i])
+	i = 0;
+	d = 0;
+	n = 0;
+	len = 0;
+	f = 0;
+	digit = 0;
+
+	while (s[len] != '\0')
+		len++;
+
+	while (i < len && f == 0)
 	{
-		if (s[i] == '_')
-			neg++;
-		else if (s[i] >= '0' && s[i] <= '9')
+		if (s[i] == '-')
+			++d;
+
+		if (s[i] >= '0' && s[i] <= '9')
 		{
-			conv = conv * 10 + s[i] - '0';
-			num = 1;
+			digit = s[i] - '0';
+			if (d % 2)
+				digit = -digit;
+			n = n * 10 + digit;
+			f = 1;
+			if (s[i + 1] < '0' || s[i + 1] > '9')
+				break;
+			f = 0;
 		}
-		else if (num == 1 && (s[i] < '0' || s[i] > '9'))
-			break;
 		i++;
 	}
-	if (neg % 2 == 0)
-		return (conv);
-	else
-		return (-conv);
+
+	if (f == 0)
+		return (0);
+
+	return (n);
 }
-
-
-
