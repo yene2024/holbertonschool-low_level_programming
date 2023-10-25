646e32a2932462cd57db21c0d884e03c22ff9abc
yene2024 <yeneishla1524@gmail.com>
2023-10-25 10:21:24 -0400

    Task 4: function that reverse

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,22 @@
+/**
+ * reverse_array - function that reverse the content
+ * @a: an array of integers
+ * @n: the number of elements to array
+ * Return: Always 0.
+ */
+
+void reverse_array(int *a, int n)
+{
+	int start = 0;
+	int end = n - 1;
+
+	while (start < end)
+	{
+		int temp = a[start];
+		a[start] = a[end];
+		a[end] = temp;
+		start++;
+		end--;
+	}
+}
+
