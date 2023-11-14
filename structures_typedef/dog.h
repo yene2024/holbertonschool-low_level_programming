d6d1947ff4861dad698fd28e119b291b2d38ea71
yene2024 <yeneishla1524@gmail.com>
2023-11-14 15:24:24 -0400

    Defining a new type struct dog

---
+++ structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifndef _HEADER_
+#define _HEADER_
+
+/**
+ * struct dog - dog attributes
+ * @name: the name of the dog
+ * @age: the age of the dog
+ * @owner: the owner of the dog
+ *
+ * decription: the attributes of a dog
+ **/
+
+struct dog
+{
+char *name;
+float age;
+char *owner
+};
+
+#endif
+
