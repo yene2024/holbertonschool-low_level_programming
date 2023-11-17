21bf08fe91a78944efae9829483966197138c4fc
yene2024 <yeneishla1524@gmail.com>
2023-11-17 16:02:37 -0400

    Writing a function that prints numbers, followed by a new line.

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -3,18 +3,7 @@
 
 #include <stdarg.h>
 
-/**
- * struct format - Struct format
- *
- * @format: The format specifier
- * @f: The function associated
- */
-typedef struct format
-{
-	char *format;
-	void (*f)(va_list args);
-} format_t;
-
-int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
 
 #endif /* VARIADIC_FUNCTIONS_H */
+
