ea5209aec0b7ec2afa1101f53d6c367442b43e4c
yene2024 <yeneishla1524@gmail.com>
2023-11-17 15:46:43 -0400

    Writing a program that performs simple operations.

--- function_pointers/3-calc.h
+++ function_pointers/3-calc.h
@@ -1,19 +1,16 @@
 #ifndef CALC_H
 #define CALC_H
-
 #include <stdio.h>
 #include <stdlib.h>
-
 /**
  * struct op - Struct op
- *
  * @op: The operator
  * @f: The function associated
  */
 typedef struct op
 {
-  char *op;
-  int (*f)(int a, int b);
+char *op;
+int (*f)(int a, int b);
 } op_t;
 
 int op_add(int a, int b);
@@ -25,4 +22,3 @@ int op_mod(int a, int b);
 int (*get_op_func(char *s))(int, int);
 
 #endif /* CALC_H */
-
