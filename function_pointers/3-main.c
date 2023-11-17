5ede0498f5fd76096ebc051cd08cc7f9daf667b1
yene2024 <yeneishla1524@gmail.com>
2023-11-17 15:34:24 -0400

    Writing a program that performs simple operations.

---
+++ function_pointers/3-main.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "3-calc.h"
+
+/**
+ * main - entry point
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0 on success, otherwise 98, 99, or 100
+ */
+int main(int argc, char *argv[])
+{
+	int num1, num2, result;
+	int (*operation)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		return (98);
+	}
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+
+	operation = get_op_func(argv[2]);
+
+	if (operation == NULL)
+		return (99);
+
+	result = operation(num1, num2);
+
+	printf("%d\n", result);
+
+	return (0);
+}
