af2de3301c7ced7bf91708121815f3fbb1ac5e26
yene2024 <yeneishla1524@gmail.com>
2023-11-17 16:23:49 -0400

    Writing a function that prints anything.

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -3,7 +3,7 @@
 
 #include <stdarg.h>
 
-void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
 
 #endif /* VARIADIC_FUNCTIONS_H */
 
