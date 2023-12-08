93c2794e9f26105ce0e4116bf46c6c5466e20fd8
yene2024 <yeneishla1524@gmail.com>
2023-12-08 10:28:41 -0400

    Writing a function that reads a text file and prints it to the POSIX standard output.

--- file_io/main.h
+++ file_io/main.h
@@ -10,8 +10,12 @@
 #include <sys/stat.h>
 #include <stddef.h>
 /**
- * All prototypes for the project
-*/
+ * read_textfile - reads a text file and prints it to the POSIX standard output
+ * @filename: Name of the file to read
+ * @letters: The number of letters to read and print
+ * Return: The number of letters read and printed, or -1 on error
+ */
+
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
