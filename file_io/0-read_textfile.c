82a4ef48d1e5643076c6555423e8e54213478c9c
yene2024 <yeneishla1524@gmail.com>
2023-12-08 10:20:33 -0400

    Writing a function that reads a text file and prints it to the POSIX standard output.

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -1,52 +1,33 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-
+#include "main.h"
 /**
- * read_textfile - Read a text file and print its content to standard output.
- * @filename: The name of the file to be read.
- * @letters: The number of letters to read and print.
- *
- * Return: The actual number of letters read and printed. 0 on failure.
- */
-
+ * read_textfile - reads a text file and prints it to the POSIX standard output
+ * @filename: Name for the file to read
+ * @letters: The Numbers of letters of that should be readed and printed
+ * Return: The numbers of letters readed and printed
+*/
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	FILE *file;
-	char buffer[1024];
-	ssize_t bytesRead, totalBytesRead = 0;
+	int wr, rd, fd;
+
+	char *buf = malloc(sizeof(char *) * letters);
+
 
 	if (filename == NULL)
-	{
 		return (0);
-	}
-
-	file = fopen(filename, "r");
-	if (file == NULL)
-	{
+	if (buf == NULL)
 		return (0);
-	}
+	fd = open(filename, O_RDONLY, 0600);
+	if (fd == -1)
+		return (0);
+	rd = read(fd, buf, letters);
+	if (rd == -1)
+		return (0);
+	wr = write(STDOUT_FILENO, buf, rd);
+	if (wr == -1)
+		return (0);
 
-	while ((bytesRead = fread(buffer, 1, sizeof(buffer), file)) > 0)
-	{
-		ssize_t bytesWritten = write(STDOUT_FILENO, buffer, bytesRead);
+	free(buf);
+	close(fd);
 
-		if (bytesWritten != bytesRead)
-		{
-			fclose(file);
-			return (0);
-		}
-
-		totalBytesRead += bytesRead;
-
-		if (totalBytesRead >= (ssize_t)letters)
-		{
-			break;
-		}
-	}
-
-	fclose(file);
-
-	return (totalBytesRead);
+	return (wr);
 }
-
